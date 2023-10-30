a7a4189e40a682e3ba36f806ba0598533bc67fd2
Umamad Mac <marandmohammad@gmail.com>
2023-10-30 17:16:53 +0330

    refactor: set global css variables

--- src/modules/SearchBar.js
+++ src/modules/SearchBar.js
@@ -1,37 +1,39 @@
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import styles from '../modules/searchbar.module.css';
-// import { useRouter } from 'next/router';
+import { useState } from "react";
+
+import { useRouter } from "next/router";
+
+import styles from "../modules/searchbar.module.css";
 
 const SearchBar = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [min ,  setMin] = useState("");
-    const [max , setMax] = useState("");
-    const searchHandler = () =>{
-        if (min && max) {
-            router.push(`/filter/${min}/${max}`)
-
-        }
-        else{
-            alert("input Min and Max Price")
-        }
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+  const searchHandler = () => {
+    if (min && max) {
+      router.push(`/filter/${min}/${max}`);
+    } else {
+      alert("input Min and Max Price");
     }
-     return (
-        <div className={styles.container}>
-         <input placeholder='Enter Min Price'
-          value={min} 
-          onChange={e => setMin(e.target.value)}
-           />
+  };
 
+  return (
+    <div className={styles.container}>
+      <input
+        placeholder="Enter Min Price"
+        value={min}
+        onChange={(e) => setMin(e.target.value)}
+      />
 
-         <input placeholder='Enter Max Price'
-          value={max}
-           onChange={e => setMax(e.target.value)}
-           />
-         <button onClick={searchHandler}>Search</button>
-        </div>
-    );
+      <input
+        placeholder="Enter Max Price"
+        value={max}
+        onChange={(e) => setMax(e.target.value)}
+      />
+
+      <button onClick={searchHandler}>Search</button>
+    </div>
+  );
 };
 
 export default SearchBar;
